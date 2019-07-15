c693c9670be811e379b6378be90b0f7aca5425a9
Jay <jvelayud@gmail.com>
2019-07-15 19:26:53 -0400

    Removing warning if no warmup plugin

--- src/ignore-warmup-plugin.js
+++ src/ignore-warmup-plugin.js
@@ -4,15 +4,17 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const config = require("./config");
 
 module.exports = function(webpackConfig) {
-  delete webpackConfig.entry["_warmup/index"];
-  webpackConfig.plugins.push(
-    new CopyWebpackPlugin([
-      {
-        from: path.join(config.servicePath, "_warmup"),
-        to: "_warmup"
-      }
-    ])
-  );
+  if (webpackConfig.entry["_warmup/index"]) {
+    delete webpackConfig.entry["_warmup/index"];
+    webpackConfig.plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: path.join(config.servicePath, "_warmup"),
+          to: "_warmup"
+        }
+      ])
+    );
+  }
 
   return webpackConfig;
 };
