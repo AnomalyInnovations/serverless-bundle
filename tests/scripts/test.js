2b60feb3dab0a43ad066ba3132cdf3f7dc975621
Jay <jvelayud@gmail.com>
2019-08-17 19:22:48 -0400

    Updating travis with new test

--- tests/scripts/test.js
+++ tests/scripts/test.js
@@ -1,3 +1,9 @@
+/**
+ * Test to make sure overriding Jest config works.
+ * ./setupTests.js is set as `setupFilesAfterEnv` in
+ * the package.json. It should be included before a
+ * test is run.
+ */
 import asyncSum from './asyncSum';
 
 function timeout(ms) {
