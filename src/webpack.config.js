2d0c4b4044a7fe0f4cfd37fe9bf6b3fac5d95c47
Jake Champion <me@jakechampion.name>
2019-11-21 17:04:00 +0000

    Change targetted node version to 10 as 8 is EOL

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -49,7 +49,7 @@ function babelLoader() {
           require.resolve("@babel/preset-env"),
           {
             targets: {
-              node: "8.10"
+              node: "10"
             }
           }
         ]
