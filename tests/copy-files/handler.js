540cd2dad3bd11de4526a56c102c386b49bca13f
Ian Dela Cruz <iandc76@gmail.com>
2021-02-28 07:33:16 +0800

    Rename test folder for packaged individually

--- tests/copy-files/handler.js
+++
@@ -1,9 +0,0 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-};
