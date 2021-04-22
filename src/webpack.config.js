0ca9841fb525140d861dcc5a614e2e602f955d54
Jay <jvelayud@gmail.com>
2021-04-21 21:54:09 -0400

    Adding cwd for eslint

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -282,7 +282,7 @@ function plugins() {
     if (ENABLE_LINTING) {
       forkTsCheckerWebpackOptions.eslint = {
         files: path.join(servicePath, "**/*.ts"),
-        options: { baseConfig: tsEslintConfig },
+        options: { cwd: servicePath, baseConfig: tsEslintConfig },
       };
     }
 
