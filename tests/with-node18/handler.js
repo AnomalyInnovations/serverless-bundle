7a060eb992340126538ee9c4ad3495f55e07f544
Christian Nuss <christian.nuss@gmail.com>
2023-11-24 11:27:13 -0500

    add a node18 test

---
+++ tests/with-node18/handler.js
@@ -0,0 +1,26 @@
+class User {
+  constructor() {
+    this.counter = 0;
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  get count() {
+    return this.counter;
+  }
+}
+
+const user = new User();
+
+export const hello = async (event) => {
+  user.increment();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Go Serverless v1.0! Your function executed successfully! Count is ${user.count}`,
+      input: event,
+    }),
+  };
+};
