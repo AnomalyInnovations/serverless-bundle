1003b923821be421002acc63f1d2a4d847f075ac
Christian Nuss <christian.nuss@gmail.com>
2023-11-24 12:20:14 -0500

    remove formatting change

--- tests/force-exclude/handler.js
+++ tests/force-exclude/handler.js
@@ -10,7 +10,7 @@ export const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event,
-    }),
+      input: event
+    })
   };
 };
