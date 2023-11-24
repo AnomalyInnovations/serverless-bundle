bb03a977f013e28b164e664a56af6d954840b434
Christian Nuss <christian.nuss@gmail.com>
2023-11-24 12:35:39 -0500

    add comment

--- tests/force-exclude/handler.js
+++ tests/force-exclude/handler.js
@@ -2,6 +2,7 @@ import sorted from "is-sorted";
 import AWS from "aws-sdk";
 
 export const hello = async (event) => {
+  // Include a dummy AWS SDK call to ensure webpack attempts to bundle it
   AWS.config.update({
     region: "us-east-1",
   });
@@ -10,7 +11,7 @@ export const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
+      input: event,
+    }),
   };
 };
