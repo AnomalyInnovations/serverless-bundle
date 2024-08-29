232c279ad7a8a6e4a090f3e24085182bfcb9d0eb
Gligor Kotushevski <gligorkot@gmail.com>
2024-08-30 11:23:31 +1200

    removing node12 tests and adding 16, 18 and 20 tests

--- tests/with-node18/with-node18.test.js
+++ tests/with-node18/with-node18.test.js
@@ -8,7 +8,7 @@ afterAll(async () => {
   await clearNpmCache(__dirname);
 });
 
-test("node 12", async () => {
+test("node 18", async () => {
   const result = await runSlsCommand(__dirname);
 
   expect(result).not.toMatch(errorRegex);
